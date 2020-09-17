f12a5e905fc0c4c6f49547486c1274f6575e02d7
Aziz Namazov <anamazov@amazon.com>
2020-09-18 00:34:51 +1000

    Lesson 17

---
+++ src/lesson17/HeapOverflow.java
@@ -0,0 +1,17 @@
+package lesson17;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Run with -Xmx1m option
+ */
+public class HeapOverflow {
+    public static List<Object> objectList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        while(true){
+            objectList.add(new Object());
+        }
+    }
+}
---
+++ src/lesson17/StackOverflow.java
@@ -0,0 +1,11 @@
+package lesson17;
+
+public class StackOverflow {
+    public static void main(String[] args) {
+        call();
+    }
+
+    private static void call(){
+        call();
+    }
+}

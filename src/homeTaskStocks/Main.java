bef148c6e8b409a67e9a40c906234361adf67db9
Aziz Namazov <anamazov@amazon.com>
2020-10-07 22:52:03 +1100

    Finalize stock demo

--- src/homeTaskStocks/Main.java
+++ src/homeTaskStocks/Main.java
@@ -1,12 +1,33 @@
 package homeTaskStocks;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         NetUtitilies netUtitilies = new NetUtitilies();
 
-        List<Stock> imbStocks =  netUtitilies.getMonthlyStocks("IBM");
-        System.out.println(imbStocks);
+        List<Stock> ibmStocks =  netUtitilies.getMonthlyStocks();
+
+        if (ibmStocks.isEmpty()){
+            System.out.println("No trades were executed so far");
+            return;
+        }
+
+        Stock lastTradedStock = ibmStocks.get(0);
+        System.out.println("Last traded stock value is " + lastTradedStock.getValue() + " at " + lastTradedStock.getDate() );
+
+        ibmStocks.sort(new Comparator<Stock>() {
+            @Override
+            public int compare(Stock o1, Stock o2) {
+                return new Float(o1.getValue()).compareTo(new Float(o2.getValue()));
+            }
+        });
+
+        Stock maxValue = ibmStocks.get(ibmStocks.size() - 1);
+        System.out.println("Max value was " + maxValue.getValue() + " at " + maxValue.getDate() );
+
+        Stock minValue = ibmStocks.get(0);
+        System.out.println("Min value was " + minValue.getValue() + " at " + minValue.getDate() );
     }
 }
--- src/homeTaskStocks/NetUtitilies.java
+++ src/homeTaskStocks/NetUtitilies.java
@@ -4,7 +4,6 @@ package homeTaskStocks;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -15,32 +14,29 @@ import java.util.Scanner;
  */
 public class NetUtitilies {
 
-    private final String BASE_URL = "https://www.alphavantage.co";
 
-    public List<Stock> getMonthlyStocks(String symbol){
-        String url = BASE_URL + "/query?function=TIME_SERIES_MONTHLY&apikey=demo&datatype=csv&symbol=" + symbol;
+    public List<Stock> getMonthlyStocks(){
+        String url = "https://raw.githubusercontent.com/productstar-team/javaTwo/master/resources/monthly_IBM.csv";
         try (BufferedInputStream inStream = new BufferedInputStream(new URL(url).openStream())) {
             Scanner s = new Scanner(inStream).useDelimiter("\\A");
             String result = s.hasNext() ? s.next() : "";
             return convertCsvToStocks(result);
 
         } catch (IOException e) {
-            System.out.println("Unable to retrieve stocks for " + symbol);
+            System.out.println("Unable to retrieve stocks for IBM");
         }
 
         return Collections.emptyList();
     }
 
     private List<Stock> convertCsvToStocks(String result) {
-        System.out.println(result);
-
         List<Stock> stocks = new LinkedList<>();
         String[] lines = result.split("\\n");
         for (int i = 1; i < lines.length; i++){
             stocks.add(convertLineToStock(lines[i]));
         }
 
-        return new ArrayList<>();
+        return stocks;
     }
 
     private Stock convertLineToStock(String line) {
